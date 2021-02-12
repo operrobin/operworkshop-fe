0a63a50987de5784deda0c697b68c0064a43fdcd
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-12 11:38:46 +0700

    Precompiled asset

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+
+/**
+ * PWA Service worker settings
+ * @link https://shareurcodes.com/blog/how-to-easily-convert-your-existing-laravel-application-into-a-progressive-web-app-by-using-workbox
+ */
+
+// Check service workers are supported
+if('serviceWorker' in navigator){
+    // Use the windows load event to keep the page load performance
+    window.addEventListener('load', () =>{
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
---
+++ resources/js/services/whatsappServices.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+
+/**
+ * Axios ApiHandler
+ */
+
+module.exports = {
+    whatsappOtp: number => {
+        let uri = "uri/api";
+
+        return axios.post(uri, {
+
+        });
+    }
+}
