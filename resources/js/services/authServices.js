d8fd756e57d281e1361d02c429bd90658c020a8d
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-02-14 17:32:47 +0700

    Added Auth and Booking services

---
+++ resources/js/services/authServices.js
@@ -0,0 +1,14 @@
+/**
+ * authServices.js
+ * 
+ * This file contains all services related to Authentication.
+ */
+
+const { default: axios } = require("axios");
+
+ class AuthServices{
+
+    async sendOtp(phone, otp){
+        return axios.get();
+    }
+ }
