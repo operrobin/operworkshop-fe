ac086193adc4506193f40d5110e3b73c72c6ac6c
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-02-14 17:32:07 +0700

    Added PhoneOTP model

---
+++ app/Model/PhoneOTP.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PhoneOTP extends Model
+{
+    const REGISTER = 1;
+    const LOGIN = 2;
+
+    protected $table = "otp_verifications";
+}
