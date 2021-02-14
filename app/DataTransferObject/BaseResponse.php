2197d2b11eb353bac7d4485c037545801dd710c6
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-02-14 17:30:43 +0700

    Added DTO for BaseResponse

---
+++ app/DataTransferObject/BaseResponse.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataTransferObject;
+
+class BaseResponse{
+    public $status = false;
+    public $code = 500;
+    public $message = "Some Message";
+    public $data = null;
+
+    public function __construct($data, $message, $code = 200){
+        $this->status = ($code > 399) ? false : true;
+        $this->code = $code;
+        $this->message = $message;
+        $this->data = $data;
+    }
+
+    public static function ok($data, $message, $code = 200){
+        return response(
+            new BaseResponse(
+                $data,
+                $message,
+                $code
+            )
+            , $code);
+    }
+
+    public static function error($message, $code = 500, $data = null){
+        return response(
+            new BaseResponse(
+                $data,
+                $message,
+                $code
+            )
+            , $code
+        );
+    }
+}
