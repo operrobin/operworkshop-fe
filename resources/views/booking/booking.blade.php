88ea3ebc168d1e44b00e598dc47aeddd75e5b0f4
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-02-14 17:30:34 +0700

    Eliminating Copy Paste Programmed

--- resources/views/booking/booking.blade.php
+++ resources/views/booking/booking.blade.php
@@ -1,7 +1,9 @@
 @extends('template.master')
 @section('title', 'Booking')
 @section('content')
-<form action="">
+
+{{-- STEP 1 - INFORMASI PENGGUNA --}}
+
     <div class="content">
         <div class="d-flex">
             <div class="flex-fill steps p-1 py-3 bg-light border step-active">
@@ -48,180 +50,155 @@
             <button type="button" class="btn btn-next btn-danger mt-5 rounded-pill w-100">selanjutnya</button>
         </div>
     </div>
-<div class="content">
-    <div class="">
-        <div class="d-flex">
-            <div class="flex-fill steps p-1 py-3 bg-light border ">
-                <div class="d-flex align-items-center">
-                    <div class="step-form">1</div>
-                    <p class="step-text">INFORMASI <br> PENGGUNA</p>
-                </div>
-            </div>
-            <div class="flex-fill steps p-1 py-3 bg-light border step-active">
-                <div class="d-flex align-items-center">
-                    <div class="step-form">2</div>
-                    <p class="step-text">INFORMASI <br> KENDARAAN</p>
-                </div>
-            </div>
-            <div class="flex-fill steps py-3 bg-light border">
-                <div class="d-flex align-items-center">
-                    <div class="step-form">3</div>
-                    <p class="step-text">INFORMASI <br> BENGKEL</p>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">JENIS KENDARAAN</label>
-            <div class="switch-field">
-                <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
-                <label for="switch_left">MOBIL</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label for="switch_right">MOTOR</label>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">MERK KENDARAAN</label>
-            <div class="switch-field merk">
-                <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
-                <label class="non-radius" for="switch_left">MOBIL</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-            </div>
-        </div>
 
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">TIPE KENDARAAN</label>
-            <input type="text" class="form-control form-border">
-        </div>
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">NOMOR POLISI</label>
-            <input type="text" class="form-control form-border">
-        </div>
+{{-- STEP 2 - INFORMASI KENDARAAN --}}
 
-        <button type="button" class="btn btn-next mb-4 btn-danger mt-5 rounded-pill w-100">selanjutnya</button>
-        </div>
-    </div>
-
-</div>
-<div class="content">
-    <div class="">
-        <div class="d-flex">
-            <div class="flex-fill steps p-1 py-3 bg-light border ">
-                <div class="d-flex align-items-center">
-                    <div class="step-form">1</div>
-                    <p class="step-text">INFORMASI <br> PENGGUNA</p>
-                </div>
-            </div>
-            <div class="flex-fill steps p-1 py-3 bg-light border ">
-                <div class="d-flex align-items-center">
-                    <div class="step-form">2</div>
-                    <p class="step-text">INFORMASI <br> KENDARAAN</p>
-                </div>
-            </div>
-            <div class="flex-fill steps py-3 bg-light border step-active">
-                <div class="d-flex align-items-center">
-                    <div class="step-form">3</div>
-                    <p class="step-text">INFORMASI <br> BENGKEL</p>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">PILIH BENGKEL</label>
-            <div class="switch-field">
-                <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
-                <label for="switch_left">BENGKEL RESMI</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label for="switch_right">BENGKEL UMUM</label>
-            </div>
-        </div>
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">MERK KENDARAAN</label>
-            <div class="switch-field merk">
-                <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
-                <label class="non-radius" for="switch_left">MOBIL</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-                <input type="radio" id="switch_right" name="switch_2" value="no" />
-                <label class="non-radius" for="switch_right">MOTOR</label>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">TIPE KENDARAAN</label>
-            <input type="text" class="form-control form-border">
-        </div>
-        <div class="form-group">
-            <label for="" class="text-muted mt-4">NOMOR POLISI</label>
-            <input type="text" class="form-control form-border">
-        </div>
-
-        <div id="show_maps" style="width: 100%;height:300px;"></div>
-
-        <p class="text-muted mt-4">LOKASI BENGKEL</p>
-
+    <div class="content">
         <div class="">
-            <div style="font-size: 13px;" class="row mx-1 my-2 card-review align-items-center active">
-                <div style="height: 100px; width: 100%; background-color: lightgray;" class="col-4"></div>
-                <div class="col">
-                    <div class="d-flex">
-                        <p class="font-weight-bold ">AUTOLUX</p>
-                        <div class="review font-weight-bold text-center ml-2">
-                            <i style="color: rgb(255, 217, 5);" class="fas fa-star"></i>
-                            4.7
-                        </div>
-
-
+            <div class="d-flex">
+                <div class="flex-fill steps p-1 py-3 bg-light border ">
+                    <div class="d-flex align-items-center">
+                        <div class="step-form">1</div>
+                        <p class="step-text">INFORMASI <br> PENGGUNA</p>
                     </div>
-                    <p style="font-size: 10px;" class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non consectetur ullam, vel in tempora nam iusto quia dolore sunt </p>
+                </div>
+                <div class="flex-fill steps p-1 py-3 bg-light border step-active">
+                    <div class="d-flex align-items-center">
+                        <div class="step-form">2</div>
+                        <p class="step-text">INFORMASI <br> KENDARAAN</p>
+                    </div>
+                </div>
+                <div class="flex-fill steps py-3 bg-light border">
+                    <div class="d-flex align-items-center">
+                        <div class="step-form">3</div>
+                        <p class="step-text">INFORMASI <br> BENGKEL</p>
+                    </div>
                 </div>
             </div>
-            <div style="font-size: 13px;" class="row mx-1 my-2 card-review align-items-center">
-                <div style="height: 100px; width: 100%; background-color: lightgray;" class="col-4"></div>
-                <div class="col">
-                    <div class="d-flex">
-                        <p class="font-weight-bold ">AUTOLUX</p>
-                        <div class="review font-weight-bold text-center ml-2">
-                            <i style="color: rgb(255, 217, 5);" class="fas fa-star"></i>
-                            4.7
-                        </div>
-
-
-                    </div>
-                    <p style="font-size: 10px;" class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non consectetur ullam, vel in tempora nam iusto quia dolore sunt </p>
+            <div class="container">
+            <div class="form-group">
+                <label for="" class="text-muted mt-4">JENIS KENDARAAN</label>
+                <div class="switch-field">
+                    <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
+                    <label for="switch_left">MOBIL</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label for="switch_right">MOTOR</label>
                 </div>
+            </div>
+            <div class="form-group">
+                <label for="" class="text-muted mt-4">MERK KENDARAAN</label>
+                <div class="switch-field merk">
+                    <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
+                    <label class="non-radius" for="switch_left">MOBIL</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label class="non-radius" for="switch_right">MOTOR</label>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label for="" class="text-muted mt-4">TIPE KENDARAAN</label>
+                <input type="text" class="form-control form-border">
+            </div>
+            <div class="form-group">
+                <label for="" class="text-muted mt-4">NOMOR POLISI</label>
+                <input type="text" class="form-control form-border">
+            </div>
+
+            <button type="button" class="btn btn-next mb-4 btn-danger mt-5 rounded-pill w-100">selanjutnya</button>
             </div>
         </div>
 
-        <button type="button" data-toggle="modal" data-target="#exampleModal" class="btn mb-4 btn-danger mt-3 py-2 rounded-pill w-100">BOOKING</button>
-        </div>
     </div>
 
-</div>
-</form>
+
+{{-- STEP 3 - INFORMASI BENGKEL --}}
+    <div class="content">
+        <div class="">
+            <div class="d-flex">
+                <div class="flex-fill steps p-1 py-3 bg-light border ">
+                    <div class="d-flex align-items-center">
+                        <div class="step-form">1</div>
+                        <p class="step-text">INFORMASI <br> PENGGUNA</p>
+                    </div>
+                </div>
+                <div class="flex-fill steps p-1 py-3 bg-light border ">
+                    <div class="d-flex align-items-center">
+                        <div class="step-form">2</div>
+                        <p class="step-text">INFORMASI <br> KENDARAAN</p>
+                    </div>
+                </div>
+                <div class="flex-fill steps py-3 bg-light border step-active">
+                    <div class="d-flex align-items-center">
+                        <div class="step-form">3</div>
+                        <p class="step-text">INFORMASI <br> BENGKEL</p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+            <div class="form-group">
+                <label for="" class="text-muted mt-4">PILIH BENGKEL</label>
+                <div class="switch-field">
+                    <input type="radio" id="switch_left" name="switch_2" value="yes" checked />
+                    <label for="switch_left">BENGKEL RESMI</label>
+                    <input type="radio" id="switch_right" name="switch_2" value="no" />
+                    <label for="switch_right">BENGKEL UMUM</label>
+                </div>
+            </div>
+
+            <div id="show_maps" style="width: 100%;height:300px;"></div>
+
+            <p class="text-muted mt-4">LOKASI BENGKEL</p>
+
+            <div class="">
+                <div style="font-size: 13px;" class="row mx-1 my-2 card-review align-items-center active">
+                    <div style="height: 100px; width: 100%; background-color: lightgray;" class="col-4"></div>
+                    <div class="col">
+                        <div class="d-flex">
+                            <p class="font-weight-bold ">AUTOLUX</p>
+                            <div class="review font-weight-bold text-center ml-2">
+                                <i style="color: rgb(255, 217, 5);" class="fas fa-star"></i>
+                                4.7
+                            </div>
+
+
+                        </div>
+                        <p style="font-size: 10px;" class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non consectetur ullam, vel in tempora nam iusto quia dolore sunt </p>
+                    </div>
+                </div>
+                <div style="font-size: 13px;" class="row mx-1 my-2 card-review align-items-center">
+                    <div style="height: 100px; width: 100%; background-color: lightgray;" class="col-4"></div>
+                    <div class="col">
+                        <div class="d-flex">
+                            <p class="font-weight-bold ">AUTOLUX</p>
+                            <div class="review font-weight-bold text-center ml-2">
+                                <i style="color: rgb(255, 217, 5);" class="fas fa-star"></i>
+                                4.7
+                            </div>
+
+
+                        </div>
+                        <p style="font-size: 10px;" class="">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non consectetur ullam, vel in tempora nam iusto quia dolore sunt </p>
+                    </div>
+                </div>
+            </div>
+
+            <button type="button" data-toggle="modal" data-target="#exampleModal" class="btn mb-4 btn-danger mt-3 py-2 rounded-pill w-100">BOOKING</button>
+            </div>
+        </div>
+
+    </div>
 
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
@@ -243,22 +220,15 @@
     </div>
 </div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-    crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
-    crossorigin="anonymous"></script>
+<script 
+    src="{{ asset('/scripts/script.js') }}">
+</script>
 
+<script 
+    src="https://maps.googleapis.com/maps/api/js?key={{ env('MIX_GOOGLE_MAPS_API_KEY') }}&callback=initMap" async defer>
+</script>
 
-<script src="{{ asset('/scripts/script.js') }}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
-<script type="text/javascript">
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('show_maps'), {
-      center: {lat: -1.035721, lng: 118.436931},
-      zoom: 5
-    });
-  }
+<script type="text/javascript"
+    src="{{ asset('/scripts/booking.js') }}">
 </script>
 @endsection
