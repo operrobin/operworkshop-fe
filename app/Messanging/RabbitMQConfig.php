8ecc14cb20bb7100bf069783013c113ba3d2670c
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-15 21:42:43 +0700

    Message Broker Template Config

---
+++ app/Messanging/RabbitMQConfig.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Messanging;
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+abstract class RabbitMQConfig{
+
+    /**
+     * @var \PhpAmqpLib\Connection\AMQPStreamConnection $con
+     */
+    protected $con;
+
+    /**
+     * @var 
+     */
+    protected $channel;
+    
+    protected function initializeConnection(){
+        $this->con = new AMQPStreamConnection(
+            env('RABBIT_MQ_SERVER_URI'),
+            env('RABBIT_MQ_SERVER_PORT'),
+            env('RABBIT_MQ_SERVER_USERNAME'),
+            env('RABBIT_MQ_SERVER_PASSWORD')
+        );
+
+        $this->channel = $this->con->channel();
+    }
+
+    protected function close(){
+        $this->channel->close();
+        $this->con->close();
+    }
+}
