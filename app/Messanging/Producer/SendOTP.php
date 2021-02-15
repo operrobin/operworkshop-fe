a00ae2c7a0fbc651fafeb91a408322ffacf7fb25
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-15 21:42:54 +0700

    SendOTP Producer

--- app/Messanging/Producer/SendOTP.php
+++ app/Messanging/Producer/SendOTP.php
@@ -2,6 +2,50 @@
 
 namespace App\Messanging\Producer;
 
-class SendOTP{
+use PhpAmqpLib\Message\AMQPMessage;
+
+use App\Messanging\RabbitMQConfig;
+
+class SendOTP extends RabbitMQConfig{
     
+    /**
+     * @static
+     * @var string
+     */
+    const SEND_OTP_PRODUCER = "BE_OTP_SEND";
+
+    public function __construct(){
+        $this->initializeConnection();
+    }
+
+    /**
+     * produce
+     * A producer to send OTP to RabbitMQ Server.
+     */
+    public function produce($customer_id, $phone, $otp_code){
+
+        /**
+         * @link https://www.rabbitmq.com/tutorials/tutorial-two-php.html
+         */
+        $this->channel->queue_declare(self::SEND_OTP_PRODUCER, false, true, false, false);
+
+        $message = new AMPQMessage(
+            (object) [
+                "customer_id" => $customer_id, 
+                "phone" => $phone,
+                "otp_code" => $otp_code
+            ],
+            [
+                "delivery_mode" => AMQPMessage::DELIVERY_MODE_PERSISTENT
+            ]
+        );
+
+        $this->channel->basic_publish(
+            $message,
+            '',
+            self::SEND_OTP_PRODUCER
+        );
+
+        $this->close();
+    }
 }
