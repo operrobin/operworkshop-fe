f918782739695d08730d5aa435908cc93b2d6907
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-15 21:40:28 +0700

    Added viewmodel for otp

--- public/scripts/otp.js
+++ public/scripts/otp.js
@@ -1,28 +1,60 @@
+/*
+ |--------------------------------------------------------------------------
+ | DOM Part
+ |--------------------------------------------------------------------------
+ |
+ | This part is used for any DOM related logic.
+ |
+ */    
+
 getCodeBoxElement = index => document.getElementById('codeBox' + index); 
 
-
-  function onKeyUpEvent(index, event) {
-    const eventCode = event.which || event.keyCode;
-    if (getCodeBoxElement(index).value.length === 1) {
-       if (index !== 4) {
-          getCodeBoxElement(index+ 1).focus();
-       } else {
-          getCodeBoxElement(index).blur();
-          // Submit code
-          console.log('submit code ');
-       }
-    }
-    if (eventCode === 8 && index !== 1) {
-       getCodeBoxElement(index - 1).focus();
-    }
+function onKeyUpEvent(index, event) {
+  const eventCode = event.which || event.keyCode;
+  if (getCodeBoxElement(index).value.length === 1) {
+     if (index !== 4) {
+        getCodeBoxElement(index+ 1).focus();
+     } else {
+        getCodeBoxElement(index).blur();
+        // Submit code
+        console.log('submit code ');
+     }
   }
-
-  function onFocusEvent(index) {
-    for (item = 1; item < index; item++) {
-       const currentElement = getCodeBoxElement(item);
-       if (!currentElement.value) {
-            currentElement.focus();
-            break;
-       }
-    }
+  if (eventCode === 8 && index !== 1) {
+     getCodeBoxElement(index - 1).focus();
   }
+}
+
+function onFocusEvent(index) {
+  for (item = 1; item < index; item++) {
+     const currentElement = getCodeBoxElement(item);
+     if (!currentElement.value) {
+          currentElement.focus();
+          break;
+     }
+  }
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | Data Part
+ |--------------------------------------------------------------------------
+ |
+ | This part is used for any Data related logic.
+ |
+ */
+
+/**
+ * Const
+ */
+const otp_phone_input = document.getElementById('otp-phone-input');
+
+function sendOTP(){
+  window.location.replace('/booking');
+
+  return 0;
+  phone = otp_phone_input.value;
+
+  new AuthServices().Authentication(phone);
+}
+
