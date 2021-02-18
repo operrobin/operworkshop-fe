f4711ad2263aaee6dcb30d3a5d2176186e069975
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-18 22:35:27 +0700

    Added (Undone Loader)

---
+++ resources/views/template/loader.blade.php
@@ -0,0 +1,10 @@
+{{-- 
+<div id="o-loader" class="">
+    
+    <div class="overlay-top"></div>
+    <div class="spinner">
+        <div class="loadingio-spinner-gear-bymcl90jkop"><div class="ldio-oklq64pmfi">
+            <div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            </div></div>
+    </div>
+</div> --}}
