ae7a6bed2232c05931018c7ab66d6aa881081b28
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-18 22:50:26 +0700

    Unfinished controller. Will be talk later about Oper Task thingy

--- app/Http/Controllers/Api/BookingController.php
+++ app/Http/Controllers/Api/BookingController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    //
+    public function makeOrder(Request $request){
+        $v = validator()->make($request->all(), [
+            "customer_name" => "required",
+            "customer_phone" => "required",
+            "customer_email" => "required",
+            "vehicle_type" => "required",
+        ]);
+    }
 }
