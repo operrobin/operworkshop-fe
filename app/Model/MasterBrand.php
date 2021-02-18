ebb6ad933684879f066659ebe0c61d0258ae0336
Yosua Kristianto <yosuakristianto754@gmail.com>
2021-02-18 22:42:41 +0700

    Added relation to MasterBrand

--- app/Model/MasterBrand.php
+++ app/Model/MasterBrand.php
@@ -10,4 +10,8 @@ class MasterBrand extends Model
     const MOTOR = 2;
 
     protected $table = "master_brands";
+
+    public function vehicle(){
+        return $this->hasOne('App\Model\VehicleModel', 'master_brand_id', 'id');
+    }
 }
