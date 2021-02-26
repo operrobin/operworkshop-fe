40e8da737eb8dceee9dc6ba263c1d6d6f0ed4927
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-02-26 20:33:09 +0700

    Added Booking pre-send message

---
+++ resources/views/booking/booking-resume.blade.php
@@ -0,0 +1,87 @@
+<div class="modal fade" id="resume-modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content modal-dialog-scrollable">
+            <div class="modal-header border-0">
+                <h5 class="modal-title font-weight-normal text-muted" id="exampleModalLabel">Ringkasan Booking</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            
+            </div>
+            <div class="modal-body mx-3">
+
+                <span class="text-muted form-title">
+                        ①  Informasi Pengguna
+                </span>
+
+                <div class="container mt-2">
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">NAMA</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">Yosua Kristianto</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">NOMOR TELEPON</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">081210315286</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">EMAIL</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">nyanya@gmail.com</span>
+                    </div>
+                </div>
+                
+                <span class="text-muted form-title">
+                    ②   Informasi Kendaraan
+                </span>
+
+                <div class="container mt-2">
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">JENIS KENDARAAN</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">MOBIL</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">MERK MOBIL</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">HONDA</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">TIPE KENDARAAN</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">CIVIC</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">NOMOR POLISI</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">B 1234 AS</span>
+                    </div>
+                </div>
+
+                <span class="text-muted form-title">
+                    ③  Informasi Bengkel
+                </span>
+
+                <div class="container mt-2">
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">Bengkel</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">MAGELANG MOTOR JAYA</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">WAKTU BOOKING</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">10/11/2021 17:11</span>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="resume-customer-fullname" class="text-muted form-title">ALAMAT PENJEMPUTAN</label>
+                        <span id="resume-customer-fullname" class="text-uppercase form-value">Di rumah gue</span>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
