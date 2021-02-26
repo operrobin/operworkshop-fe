2f63e549894e367ddfb3a15c8c1704a878eb03c7
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-02-26 20:31:46 +0700

    Completting Booking Backend Process

---
+++ app/Helper/MessageHelper.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Helper;
+
+use App\Services\TelegramServices;
+
+use Mail;
+
+class MessageHelper{
+
+    const EMAIL = 'email';
+    const WHATSAPP = 'whatsapp';
+
+    /**
+     * sendMessage
+     * A function that help process of sending message to destinated phone number
+     * 
+     * @param string type
+     * Either 'email' or 'whatsapp'
+     * 
+     * @param string credential
+     * Destinated message. Could be an email, or phone number.
+     * 
+     * @param string message
+     * Message string. If you're using email, you must pass view's string Laravel path
+     * since we return view for SMTP. 
+     * 
+     * @param mixed object (nullable)
+     * Just view's parameters if you're using email.
+     * 
+     * @return void
+     * 
+     * @return boolean false if the type is not supported 
+     */
+    public function sendMessage($type, $destination, $message, $object = null, $subject = null){
+
+        switch(strtolower($type)){
+            case self::EMAIL: 
+                $to_name = "Customer {$destination}";
+                $to_email = $destination;
+
+                Mail::send($message, $object, function($mes) use ($to_name, $to_email, $subject) {
+                    $mes->to($to_email, $to_name)
+                        ->subject($subject);
+
+                    $mes->from(env('MAIL_USERNAME'), $subject);
+                });
+                
+                break;
+
+            case self::WHATSAPP: 
+                
+                if(env('OTP_MODE') == 'FAKE'){
+                    $this->fakeWhatsappMessanging($destination, $message);
+                }else{
+                    echo "Did I just send it to Whatsapp?";
+                }
+                break;
+
+            default: 
+                return false;
+                break;
+        }
+
+        return 'void';
+    }
+
+    /**
+     * fakeWhatsappMessanging
+     * A function that send message trough Telegram instead of Whatsapp.
+     * 
+     * @param string phone_number
+     * @param string message
+     */
+    private function fakeWhatsappMessanging($phone_number, $message){
+        $telegram = new TelegramServices();
+        $telegram->sendMessage(
+            "Dear {$phone_number},\n\n  {$message}"
+        );
+    }
+}
