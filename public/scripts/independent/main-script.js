ffdae880d9a50e2ad1ae283f278fef9beaeb37df
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-03-05 18:18:55 +0700

    Delete Dead Code

--- public/scripts/independent/main-script.js
+++ public/scripts/independent/main-script.js
@@ -19,107 +19,29 @@ function initMap()
    
 }
 
-function submitcomment()
-{
+try{
+    document.getElementById('batalkan-btn').addEventListener('click', (e) => {
+        $('#confirm_popup_cancel').modal('show');
+    });
     
-    var feedback = document.getElementById("feedback").value;
-    var kodebooking = document.getElementById("kodebooking").value;
-    var lokasibooking = document.getElementById("lokasibooking").value;
-    var waktubooking = document.getElementById("waktubooking").value;
-
-    document.getElementById("loader").style.display = "block";
-    document.getElementById("buttonya").style.display = "none";
-    document.getElementById("buttontidak").style.display = "none";
-    
-    var xhttp = new XMLHttpRequest();
-          xhttp.onreadystatechange = function() 
-            {
-                if (this.readyState == 4 && this.status == 200)
-                {
-                    window.location = "https://booking.oper.co.id/customer/pesan.php?stats=1";
-                   
-                }
-          };
-        xhttp.open("POST", "submit_comment.php", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xhttp.send("feedback="+feedback+"&kodebooking="+kodebooking+"&lokasibooking="+lokasibooking+"&waktubooking="+waktubooking);
-       
-    
+    document.getElementById('buttontidakcancel').addEventListener('click', (e) => {
+        $('#confirm_popup_cancel').modal('hide');
+    });
+}catch(e){
+    console.log(e);
 }
 
-function batalkanbooking()
-{
+try{
+    document.getElementById('selesai-btn').addEventListener('click', (e) => {
+        $('#confirm_popup').modal('show');
+    });
     
-    var alasan = document.getElementById("reason").value;
-    var kodebooking = document.getElementById("kodebooking").value;
-    var idorderopertask = document.getElementById("idorderopertask").value;
-    var idlokasi = document.getElementById("idlokasi").value;
-    var bookingstats = document.getElementById("bookingstats").value;
-    
-    if(bookingstats == "0")
-        {
-    
-    document.getElementById("loader_cancel").style.display = "block";
-    document.getElementById("buttonyacancel").style.display = "none";
-    document.getElementById("buttontidakcancel").style.display = "none";
-    
-    
-   
-    
-    var xhttp = new XMLHttpRequest();
-          xhttp.onreadystatechange = function() 
-            {
-                if (this.readyState == 4 && this.status == 200)
-                {
-                    
-                    if(this.response == "1")
-                        {
-                            window.location = "https://booking.oper.co.id/customer/pesan.php?stats=4";    
-                            
-                        }
-                    else if (this.response == "2")
-                        {
-                             document.getElementById("loader_cancel").style.display = "none";
-                            document.getElementById("buttonyacancel").style.display = "block";
-                            document.getElementById("buttontidakcancel").style.display = "block";
-                            document.getElementById("confirm_popup_cancel").style.display = "none";
-                            document.getElementById("bookingstats").value = "1";
-                            document.getElementById("reason").value = "";
-                            setTimeout( function ( ) { alert("Booking sudah dalam proses dan tidak dapat dibatalkan"); }, 1000 );
-                            
-                            
-                            
-                        }
-                    else{
-                         document.getElementById("loader_cancel").style.display = "none";
-                            document.getElementById("buttonyacancel").style.display = "block";
-                            document.getElementById("buttontidakcancel").style.display = "block";
-                            document.getElementById("confirm_popup_cancel").style.display = "none";
-                            document.getElementById("reason").value = "";
-                        alert("Silahkan coba lagi");
-                        
-                    }
-                      //
-                   
+    document.getElementById('buttontidak').addEventListener('click', (e) => {
+        $('#confirm_popup').modal('hide');
+    });
+}catch(e){
+    console.log(e);
+}
 
-                }
-          };
-        xhttp.open("POST", "booking_cancel.php", true);
-        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-     xhttp.send("alasan="+alasan+"&kodebooking="+kodebooking+"&idopertask="+idorderopertask+"&idlokasi="+idlokasi);
-}
-    else{
-        
-         document.getElementById("loader_cancel").style.display = "none";
-                            document.getElementById("buttonyacancel").style.display = "block";
-                            document.getElementById("buttontidakcancel").style.display = "block";
-                            document.getElementById("confirm_popup_cancel").style.display = "none";
-                            document.getElementById("bookingstats").value = "1";
-                            document.getElementById("reason").value = "";
-                            setTimeout( function ( ) { alert("Booking sudah dalam proses dan tidak dapat dibatalkan"); }, 1000 );
-        
-        
-    }
-    
-}
+
 
