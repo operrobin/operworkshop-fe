13ad634627c40cab680f6491f2ef81087ae7c362
CuaMcCarsaree44 <yosua_kristianto144@outlook.com>
2021-03-12 12:31:50 +0700

    Parse Image

--- app/Model/CmsUser.php
+++ app/Model/CmsUser.php
@@ -15,6 +15,23 @@ class CmsUser extends Model
     ];
 
     public function getImageUrlAttribute(){
-        return env('CMS_URL')."files/cms-user/".$this->url_image;
+
+        /**
+         * Manual parsing saved image url
+         */
+        $text_length = strlen($this->url_image);
+        $index = -1;
+        $file_name_length = 1;
+
+        for($i = $text_length; $i > 0; $i--){
+            if($this->url_image[$i-1] == '/'){
+                $index = $i-1;
+                break;
+            }
+
+            $file_name_length++;
+        }
+
+        return env('CMS_URL')."files/cms-user/".substr($this->url_image, $index, $file_name_length);
     }
 }
